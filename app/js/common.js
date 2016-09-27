cf2ce66b4040dc8c0871093a5ab481c554ef14e5
Andrii Melnichuk <andrewlutsk88@gmail.com>
2016-09-27 18:33:05 +0300

    make slider and booking

--- app/js/common.js
+++ app/js/common.js
@@ -18,4 +18,53 @@
 		}
   	);
 
+    var dateFormat = "dd/mm/yy",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "0",
+          changeMonth: true,
+          numberOfMonths: 3,
+          dateFormat: dateFormat,
+          minDate: new Date()
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "0",
+        changeMonth: true,
+        numberOfMonths: 3,
+        dateFormat: dateFormat,
+        minDate: new Date()
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+ 
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+ 
+      return date;
+    }
+
+	 $('.bxslider').bxSlider({
+      mode: 'horizontal',
+      auto: true,
+      pause: 3000,
+      autoHover: true,
+      controls: true,
+      responsive: true,
+      slideWidth: 1400,
+      minSlides: 1,
+      maxSlides: 1,
+      moveSlides: 1,
+      slideMargin: 0, 
+      captions: true
+    });
+
 });
